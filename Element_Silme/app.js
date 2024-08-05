d928e4f7fbb736d2039d2996b13dff289dc13a95
Enes BULUT <bulut0258@gmail.com>
2024-08-05 16:36:06 +0300

    Dom ile  elementleri silme

---
+++ Element_Silme/app.js
@@ -0,0 +1,24 @@
+//_ Element Silme
+const todoList = document.querySelector(".list-group");
+const todos = document.querySelectorAll(".list-group-item");
+const todo1 = document.querySelector(".list-group-item");
+
+
+
+// todos[0].remove(); // 0. indexteki elementi siler.
+// todoList.removeChild(todos[1]); // 2. indexteki elementi siler.
+
+// todos[todos.length-1].remove(); //son child'ı siler.
+
+// todo1.remove(); //ilk child'ı siler.
+
+// todoList.removeChild(todos[0]); //todolist içerisindeki 0'nci indeks deki elementi siler.
+
+// todoList.removeChild(todoList.lastElementChild); //4. elementi siler
+
+console.log(todos); // Silinen elementleri gösterir.
+
+
+
+
+
