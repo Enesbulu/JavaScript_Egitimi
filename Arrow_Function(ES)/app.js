e0274d018e252ad29b1e2b960c754207a4887da9
Enes BULUT <bulut0258@gmail.com>
2024-08-09 15:51:53 +0300

    Using Arrow Funtion - ES

---
+++ Arrow_Function(ES)/app.js
@@ -0,0 +1,36 @@
+///* Arrow Function
+
+// const yazdir = () => {
+//     console.log('Hello World!');
+// }
+
+// yazdir();
+
+//-------
+// const yazdir = (firtname,lastname) => {
+//     console.log(`Merhaba ${firtname} ${lastname}`);
+// }
+
+// yazdir('Enes', 'bulut');
+
+//----------
+
+// const yazdır = (name) => console.log(`Merhaba ${name}`);
+// yazdır("Enes");
+
+//-------
+
+// const yazdir = firtname => console.log(`Merhaba ${firsname}`);
+// yazdir("Enes");
+
+//--------
+// const kupAl = x => x * x * x;
+// console.log( kupAl(3) );
+
+//-------
+
+document.addEventListener("click",()=> console.log(`Tıklanıldı `));
+
+
+
+
