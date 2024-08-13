960266b6b3ba97a7fd1040af2e117d6c16b7d2b3
Enes BULUT <bulut0258@gmail.com>
2024-08-13 14:19:20 +0300

    Using Template Riterals

---
+++ Template_Literals/app.js
@@ -0,0 +1,15 @@
+//_ Template Literals
+
+function write(firstaname, lastname){
+    // console.log("İsim: " + firstaname + " soyisim:  " + lastname);  //eski yöntem kullanım 
+    console.log(`Merhaba, ben ${firstaname} ${lastname} adlı insan.`);  //Bu şekilde kullanım sağlanır
+}
+
+
+
+write("Enes", "Bulut");
+
+
+
+
+
