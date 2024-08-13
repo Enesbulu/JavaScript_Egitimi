589c42bd27c1bbcba3f6e19fabee04e51da08175
Enes BULUT <bulut0258@gmail.com>
2024-08-13 16:07:12 +0300

    This, super, super() examples

--- Inheritanse/app.js
+++ Inheritanse/app.js
@@ -4,16 +4,63 @@ class Person {
     firstname = "Enes";
 
     write() {
-        console.log(this.firstname);
+        console.log(this.firstname);    //bu classı gösterir
     }
 }
 
-class Student extends Person {  //* genişletme - miras alma yapılışı
+//#region Inheritance Giriş
+// class Student extends Person {  //* genişletme - miras alma yapılışı
+//     write() {
+//         console.log("Person sınıfından geldi: ", this.firstname);    //* mevcut class daki özellik kullanımı -- this.
+//         console.log(super.write()); //* üst sınıf/miras alınan sınıftaki özellik kullanılır. -- super.
+//     }
+// }
+
+// const student1 = new Student();
+// student1.write();
+//#endregion
+
+
+//#region This - Super - Super()
+
+class Student2 extends Person {
+    constructor(firstname, grade) {
+        super(firstname);  //* miras alınan sınıftaki constructor'ı çağırır.
+    }
     write() {
-        console.log("Person sınıfından geldi: ", this.firstname);    //* mevcut class daki özellik kullanımı -- this.
-        console.log(super.write()); //* üst sınıf/miras alınan sınıftaki özellik kullanılır. -- super.
+        super.write(); //*Miras alınan sınıfı gösterir.
     }
 }
 
-const student1 = new Student();
-student1.write();
+const student = new Student2();
+student.write();
+
+
+
+
+
+class Person2 {
+    constructor(firstname, lastname, salary) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.salary = salary;
+    }
+    writeInfo() {
+        console.log(`Ad: ${this.firstname}, Soyad: ${this.lastname}, Maaş: ${this.salary}`);
+    }
+}
+
+
+class Student3 extends Person2 {
+    constructor(firstname, lastname, salary) {
+        super(firstname,lastname,salary); 
+    }
+    writeInfo(){
+        super.writeInfo(); //*Miras alı
+    }
+}
+
+const student2 = new Student3("Enes", "Bulut", 35000);
+student2.writeInfo();
+
+//#endregion
